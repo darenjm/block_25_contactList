2641cf89b7c7099bb8ca07b0843a969c6d74bf02
Daren Morreale <daren@givelegacy.com>
2025-02-27 17:45:11 -0500

    styled table a little

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -12,22 +12,24 @@ export default function ContactList() {
   const [contacts, setContacts] = useState(dummyContacts);
   console.log("Contacts: ", contacts);
   return (
-    <table>
-      <thead>
-        <tr>
-          <th colSpan="3">Contact List</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Phone</td>
-        </tr>
-        {contacts.map((contact) => {
-          return <ContactRow key={contact.id} contact={contact} />;
-        })}
-      </tbody>
-    </table>
+    <div className="theTable">
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="3">Contact List</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+          </tr>
+          {contacts.map((contact) => {
+            return <ContactRow key={contact.id} contact={contact} />;
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
